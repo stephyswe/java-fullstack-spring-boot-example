b064b65a9fcf435baf2ae4130626c5309c5b3f79
Stephanie <stephaniiehallberg@gmail.com>
2023-10-17 15:31:50 +0200

    44. API endpoint

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -2,6 +2,9 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -35,8 +38,18 @@ public class Main {
 
     }
     public static void main(String[] args) {
-        System.out.println(customers);
         SpringApplication.run(Main.class, args);
+    }
+
+    /*
+    @RequestMapping
+            (path = "api/v1/customer",
+                    method = RequestMethod.GET
+            )
+     */
+    @GetMapping("/api/v1/customer")
+    public List<Customer> getCustomers() {
+        return customers;
     }
 
     static class Customer {
