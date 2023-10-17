b485839485ab0c0ce3c2a3d618800b2c0691fe35
Stephanie <stephaniiehallberg@gmail.com>
2023-10-17 17:05:05 +0200

    52. creating beans

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -3,9 +3,12 @@ package com.amigoscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
+
+
 
     public static void main(String[] args) {
         /*
@@ -16,13 +19,22 @@ public class Main {
                 new CustomerController(customerService);
          */
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+        printBeans(applicationContext);
+    }
 
+    @Bean("foo")
+    public Foo getFoo() {
+        return new Foo("bar");
+    }
+
+    record Foo(String name){}
+
+    private static void printBeans(ConfigurableApplicationContext ctx) {
         String[] beanDefinitionNames =
-                applicationContext.getBeanDefinitionNames();
-
+                ctx.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             System.out.println(beanDefinitionName);
-            
+
         }
     }
 }
