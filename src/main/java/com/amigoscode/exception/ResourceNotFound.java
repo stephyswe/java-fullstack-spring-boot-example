4fc67be41c01f591f89e140adcdf5d6256ae1d97
Stephanie <stephaniiehallberg@gmail.com>
2023-10-17 17:14:12 +0200

    55. @responsestatus

--- src/main/java/com/amigoscode/exception/ResourceNotFound.java
+++ src/main/java/com/amigoscode/exception/ResourceNotFound.java
@@ -1,5 +1,9 @@
 package com.amigoscode.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFound extends  RuntimeException {
     public ResourceNotFound(String message) {
         super(message);
