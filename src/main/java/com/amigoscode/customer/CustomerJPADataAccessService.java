6cead84b1f124ecdb0c1dc7153a053e76b17a469
Stephanie <stephaniiehallberg@gmail.com>
2023-10-18 12:33:45 +0200

    80-84. Insert customer Changing CustomerDao Interface / Override insert customer / Business Logic for Adding new Customer / Controller @RequestMapping and @RequestBody / Testing POST Request

--- src/main/java/com/amigoscode/customer/CustomerJPADataAccessService.java
+++ src/main/java/com/amigoscode/customer/CustomerJPADataAccessService.java
@@ -23,4 +23,17 @@ public class CustomerJPADataAccessService implements CustomerDao {
     public Optional<Customer> selectCustomerById(Integer id) {
         return customerRepository.findById(id);
     }
+
+    @Override
+    public void insertCustomer(Customer customer) {
+        customerRepository.save(customer);
+
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return customerRepository.existsCustomerByEmail(email);
+    }
+
+
 }
